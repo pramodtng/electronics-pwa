f1f33e0052fcff0698a3eed8d1a716f017d9e0a2
pramodtng <psingay96@gmail.com>
2023-01-11 15:17:39 +0600

    changes

--- components/AllPhones.js
+++ components/AllPhones.js
@@ -7,7 +7,7 @@ const AllPhones = ({ phones }) => {
   return (
     <div className='bg-white'>
       <div className='mx-auto max-w-2xl py-2 px-4 sm:px-6 lg:max-w-7xl lg:px-4'>
-        <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+        <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8'>
           {
             phones.data.map(function (datas) {
               return (
