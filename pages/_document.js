e5b2fdafa3070da99ffa8ea6a3e383cc48ccc445
pramodtng <psingay96@gmail.com>
2023-01-11 14:16:13 +0600

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, Link } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-72x72.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <body>
         <Main />
         <NextScript />
